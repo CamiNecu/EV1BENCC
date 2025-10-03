8e413e72f0480f58714a62b306e3d74cb0f3217c
CamiNecu <CamiloNecu@outlook.es>
2025-10-02 22:48:55 -0300

    cambios a url a base y settings

--- noticias/views.py
+++ noticias/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Noticia
 def Noticias(request):
     noticia=Noticia.objects.all()
-    return render(request,"posteo/noticias.html",
+    return render(request,"noticias/noticias.html",
                   {"noticia":noticia})
