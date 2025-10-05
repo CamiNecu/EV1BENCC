6ef3e3a99333920f4a39716f84f0189beeca3b4f
CamiNecu <CamiloNecu@outlook.es>
2025-10-04 21:35:14 -0300

    Paginacion de noticias

--- noticias/views.py
+++ noticias/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Noticia
+
 def Noticias(request):
-    noticia=Noticia.objects.all()
-    return render(request,"noticias/noticias.html",
-                  {"noticia":noticia})
+    noticias_list = Noticia.objects.all()
+    paginator = Paginator(noticias_list, 1)  # 1 noticias por página
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "noticias/noticias.html", {"page_obj": page_obj})
